6c44f02a47f3cdd3cc880a8fa295497c5c744ea4
yctrl <tictrl81@gmail.com>
2021-08-26 10:57:33 +0900

    init2

---
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -0,0 +1,32 @@
+package com.godcoder.myhome.controller;
+
+import com.godcoder.myhome.Models.Board;
+import com.godcoder.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
+        return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model){
+        return "board/form";
+    }
+
+}
---
+++ src/main/java/com/godcoder/myhome/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.godcoder.myhome.repository;
+
+import com.godcoder.myhome.Models.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+
+}
