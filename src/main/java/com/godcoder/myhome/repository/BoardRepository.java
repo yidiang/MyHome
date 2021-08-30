e48f123af744a7346cd821f24814fa494a678a96
yctrl <tictrl81@gmail.com>
2021-08-30 11:09:40 +0900

    init3

--- src/main/java/com/godcoder/myhome/repository/BoardRepository.java
+++ src/main/java/com/godcoder/myhome/repository/BoardRepository.java
@@ -3,7 +3,10 @@ package com.godcoder.myhome.repository;
 import com.godcoder.myhome.Models.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+import java.util.List;
 
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 
 }
